291d4f105c3d5e14fd050c5f860abfdc9edd3c6d
Davide Brunetti <davide.brunetti@novetica.org>
2016-11-25 16:30:29 +0000

    Implemented substitute function.

--- sessionTipes.hs
+++ sessionTipes.hs
@@ -1,5 +1,5 @@
 
-type Val = Int
+type Val = Int | Name 
 
 type Var = Name
 
@@ -16,18 +16,31 @@ type Channel = (Polarity, Name)
 (-=) Minus Minus = True
 (-=) _ _ = False 
 
-eq_channel (p, name) (p1, name1) = if ((opposite p) -= p1 ) then True
+eq_channel (p, name) (p1, name1) = if (((opposite p) -= p1 ) && (name1==name)) then True
                                    else False
 
 data Label = String deriving (Show, Eq)
 
-data Process = Zero | Input Channel Var Process | Output Channel Val Process |
+data Process = Zero | Input Channel Var Process | Output Channel Val Process |	--Input aspetta un input
                Select Channel Label Process | Branch Channel [(Label, Process)]
                deriving (Show) 
 
-exec Zero Zero = Zero 
-exec (Output ch var pr ) (Input ch1 val pr1 ) = if (eq_channel ch ch1) then exec pr pr1
-                                                else Zero
-exec (Select ch la pr ) (Branch ch1 []) = Zero
-exec (Select ch la pr ) (Branch ch1 (a:as)) = if ((fst a) == la) && (eq_channel ch ch1) then exec pr (snd a)
-                                              else exec (Select ch la pr ) (Branch ch1 as)
+substitute Zero val var = Zero
+substitute (Input ch1 var1 pr1 ) val var = substitute pr1 val var
+substitute (Output ch1 val1 pr1 ) val var = if val1 == var then do {val1 = val; substitute pr1 val var }  --side effect?? ??
+                                            else substitute pr1 val var
+substitute (Select ch1 la1 pr1 ) val var = substitute pr1 val var
+substitute (Branch ch1 as ) val var = map substitute (map snd as )
+
+dual (Output ch val pr ) (Input ch1 var pr1 ) = if (eq_channel ch ch1) then True else False
+dual (Select ch la pr ) (Branch ch1 []) = False
+dual (Select ch la pr ) (Branch ch1 (a:as)) =  if ((fst a) == la) && (eq_channel ch ch1) then True 
+	                                           else dual (Select ch la pr ) (Branch ch1 as)
+dual _ _  = False
+
+reducePList [] = []
+reducePList as = map exec [ (x,y) | x<-as ,y<-as, dual x y == True ] 
+
+exec ( (Output ch val pr ) , (Input ch1 var pr1 ) ) = (substitute pr1 val var) pr
+exec ( (Select ch la pr ) , (Branch ch1 (a:as)) ) = if ((fst a) == la) then pr (snd a)
+	                                              	else dual (Select ch la pr ) (Branch ch1 as)
