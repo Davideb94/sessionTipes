5a4d33792dce86056296784a2a053a66a3e1c99b
Davide Brunetti <davide.brunetti@novetica.org>
2016-11-29 16:43:00 +0000

    Refactored functions.

--- sessionTipes.hs
+++ sessionTipes.hs
@@ -1,9 +1,9 @@
 
-type Val = Int | Name 
-
-type Var = Name
-
 type Name = String
+
+type Var = Name 
+
+data Val = Int | Name deriving (Show, Eq)
 
 data Polarity = Plus | Minus deriving (Show)
 
@@ -25,22 +25,31 @@ data Process = Zero | Input Channel Var Process | Output Channel Val Process |	-
                Select Channel Label Process | Branch Channel [(Label, Process)]
                deriving (Show) 
 
+getChannel (Output ch val pr ) = ch
+getVal (Output ch val pr ) = val
+getProcess (Output ch val pr ) = pr
+
 substitute Zero val var = Zero
 substitute (Input ch1 var1 pr1 ) val var = substitute pr1 val var
-substitute (Output ch1 val1 pr1 ) val var = if val1 == var then do {val1 = val; substitute pr1 val var }  --side effect?? ??
-                                            else substitute pr1 val var
 substitute (Select ch1 la1 pr1 ) val var = substitute pr1 val var
-substitute (Branch ch1 as ) val var = map substitute (map snd as )
+substitute (Branch ch1 as ) val var = ( Branch ch1 (zip [ ys  | ys<-(map fst as) ] [(substitute xs val var) | xs<-(map snd as)]) )
+substitute (Output ch1 val1 pr1 ) val var = if val1 == var then (Output ch1 val (substitute pr1 val var))
+                                            else substitute pr1 val var
 
+dual (Input ch1 var pr1) (Output ch val pr) = dual (Output ch val pr ) (Input ch1 var pr1 )
+dual (Branch ch1 (a:as)) (Select ch la pr ) = dual (Select ch la pr ) (Branch ch1 (a:as))
 dual (Output ch val pr ) (Input ch1 var pr1 ) = if (eq_channel ch ch1) then True else False
 dual (Select ch la pr ) (Branch ch1 []) = False
 dual (Select ch la pr ) (Branch ch1 (a:as)) =  if ((fst a) == la) && (eq_channel ch ch1) then True 
 	                                           else dual (Select ch la pr ) (Branch ch1 as)
 dual _ _  = False
 
-reducePList [] = []
-reducePList as = map exec [ (x,y) | x<-as ,y<-as, dual x y == True ] 
+pList [] = []
+pList (a:as) = reducePList a as : pList as 
 
-exec ( (Output ch val pr ) , (Input ch1 var pr1 ) ) = (substitute pr1 val var) pr
-exec ( (Select ch la pr ) , (Branch ch1 (a:as)) ) = if ((fst a) == la) then pr (snd a)
-	                                              	else dual (Select ch la pr ) (Branch ch1 as)
+reducePList x [] = []
+reducePList x (a:as) = if (dual x a) then exec x a else reducePList x as  
+
+exec (Output ch val pr ) (Input ch1 var pr1 ) = [(substitute pr1 val var), pr]
+exec (Select ch la pr ) (Branch ch1 (a:as)) = if ((fst a) == la) then [pr, (snd a)]
+	                                              	else exec (Select ch la pr ) (Branch ch1 as)
